044263768c49edcf59d517e00aa29f9d8ef418cf
Abhilash Mishra <n.arrow001@gmail.com>
2021-10-30 08:50:23 +0530

    created layouts

---
+++ app/src/main/java/com/abhilashmishra/repodisplay/adapter/PRListAdapter.kt
@@ -0,0 +1,20 @@
+package com.abhilashmishra.repodisplay.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.abhilashmishra.repodisplay.R
+import com.abhilashmishra.repodisplay.model.PRModel
+
+class PRListAdapter(private val arrayList: ArrayList<PRModel>) : RecyclerView.Adapter<ClosedPRViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ClosedPRViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_pull_request, parent)
+        return ClosedPRViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ClosedPRViewHolder, position: Int) {
+        holder.populateView(arrayList[position])
+    }
+
+    override fun getItemCount(): Int = arrayList.size
+}
